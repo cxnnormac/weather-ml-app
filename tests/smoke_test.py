2f7a1210c0462a882b788a97d5e8c959594679f5
Ubuntu <ubuntu@ip-172-31-82-77.ec2.internal>
2025-11-02 19:43:58 +0000

    Fix: applied changes from (a) to (d)

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -9,12 +9,13 @@ class TestAppSmoke(unittest.TestCase):
 	# Complete the function below to test a success in running the application
 	def test_prediction_route_success(self):
 		response = self.client.get('/')
+		self.assertEqual(response.status_code, 200)
 		
 
 	# Complete the function below to test a form is rendered
 	def test_get_form(self):
 		response = self.client.get('/')
-		
+		self.assertIn(b"<form", response.data)
  
 if __name__ == '__main__':
 	unittest.main()
